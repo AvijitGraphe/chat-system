ef555c372cc2080ca0a6d34c5293778afb5ee462
AvijitGraphe <avijit.malik.graphe@gmail.com>
2024-11-20 18:37:51 +0530

    deployed the client side

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,12 +1,12 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  webpack: function(config, env) {
-    config.output.path = path.resolve(__dirname, '../backend/build');
-    return config;
-  },
-  paths: function(paths, env) {
-    paths.appBuild = path.resolve(__dirname, '../backend/build');
-    return paths;
-  }
-};
+// module.exports = {
+//   webpack: function(config, env) {
+//     config.output.path = path.resolve(__dirname, '../backend/dist');
+//     return config;
+//   },
+//   paths: function(paths, env) {
+//     paths.appBuild = path.resolve(__dirname, '../backend/dist');  
+//     return paths;
+//   }
+// };
